e1c3fac9fd1b89d43531c812e53af788f5fbb7c8
Joey Robinson <theamazingness@gmail.com>
2020-03-25 16:34:37 -0400

    Adjusting for build.

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `${process.env.GOOGLE_ANALYTICS_ID}`,
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
       },
     },
     `gatsby-plugin-feed`,
