03eebbf7bad5710171f4cb341baaca1381aefaa5
Joey Robinson <theamazingness@gmail.com>
2020-03-25 22:32:57 -0400

    pls a11y.

--- src/templates/recipe.template.js
+++ src/templates/recipe.template.js
@@ -15,30 +15,20 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
         description={post.frontmatter.description || post.excerpt}
       />
       <section className="recipe">
-        {/* <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul> */}
+        <span className="recipe--previous">
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </span>
+        <span className="recipe--next">
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
+            </Link>
+          )}
+        </span>
 
         <div className="recipe--container">
           <hgroup>
