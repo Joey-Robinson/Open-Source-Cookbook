25098a52d1bd7d5c8eb48deedd390b6925b7ded2
Joey Robinson <theamazingness@gmail.com>
2020-03-27 23:35:05 -0400

    More media queries. I'm going to go mobile first next time. This is garbage.

--- src/pages/howto.js
+++ src/pages/howto.js
@@ -7,17 +7,16 @@ const Howto = () => (
   <Layout>
     <SEO title="How-To" />
     <seciton className="howto">
-      <h2>Here we have 2 options!</h2>
       <div className="howto--main">
         <p>
-          We can either go to the <Link to="/howto/text/">HowTo - Text</Link>{" "}
-          guide and read along with the text.
+          You can view the text guide by{" "}
+          <Link to="/howto/text/">Clicking Here</Link>
         </p>
-
         <p>
-          Or, we can go to the <Link to="/howto/images/">HowTo - Images</Link>{" "}
-          guide and follow the images.
+          Or you can view the image guide by{" "}
+          <Link to="/howto/images/">Clicking Here instead.</Link>
         </p>
+        <p>You can also scroll down for both!</p>
       </div>
     </seciton>
   </Layout>
