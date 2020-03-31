516ebf2cbe9accb6bada13b7b51bfa0da9f456fd
Joey Robinson <theamazingness@gmail.com>
2020-03-30 23:55:01 -0400

    Styling adjustments.

--- src/components/header/header.main.js
+++ src/components/header/header.main.js
@@ -1,15 +1,28 @@
 import React from "react"
 import Navigation from "./header.nav"
+import useDarkMode from "use-dark-mode"
 
-const Header = () => (
-  <header className="header">
-    <hgroup className="header--group">
-      <h1>Open-Source Cookbook</h1>
-      <h4>Share recipes. Learn git.</h4>
-    </hgroup>
-    <br />
-    <Navigation />
-  </header>
-)
+const Header = () => {
+  const darkMode = useDarkMode(false)
 
+  return (
+    <header className="header">
+      <div id="dark" className="header--dark">
+        <button type="button" onClick={darkMode.disable}>
+          Light Mode
+        </button>
+        <button type="button" onClick={darkMode.enable}>
+          Dark Mode
+        </button>
+      </div>
+
+      <hgroup className="header--group">
+        <h1>Open-Source Cookbook</h1>
+        <h4>Share recipes. Learn git.</h4>
+      </hgroup>
+      <br />
+      <Navigation />
+    </header>
+  )
+}
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,11 @@
 import React from "react"
 import Header from "./header/header.main"
-import DarkModeToggle from "./darkmode/dark.darkmode"
 
 // prettier-ignore
 const Layout = ({children}) => (
   // prettier-ignore
   <>
     <main className="primary">
-      <DarkModeToggle />
       <Header />
       {children}
     </main>
